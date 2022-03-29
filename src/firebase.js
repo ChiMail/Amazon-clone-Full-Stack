809f18b3cf90a57efcbc8f18d0d25c72b3225e22
ChiMail <1987c1112@gmail.com>
2022-03-29 10:16:01 -0400

    Added Checkout Page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC11LFqeJl4t8w2G8neb7y6YNMYoP1MXAg",
+    authDomain: "clone-7b82b.firebaseapp.com",
+    projectId: "clone-7b82b",
+    storageBucket: "clone-7b82b.appspot.com",
+    messagingSenderId: "975541319821",
+    appId: "1:975541319821:web:e62a49b3ace3fa63224ac4",
+    measurementId: "G-LQZP6JQCS7"
+  };
